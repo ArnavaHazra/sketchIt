51fba5f87b8b10b7e273acbc3ff487aed3bb0e7d
arohaz2005 <arohaz085@gmail.com>
2026-01-15 18:37:07 +0530

    zod

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,12 +1,21 @@
 import express from "express"
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "./config.js";
+import { JWT_SECRET } from "@repo/backend-common/config"
 import { middleware } from "./middleware.js";
+import { CreateUserSchema, CreateRoomSchema, SigninSchema } from "@repo/common/types"
 
 const PORT = 3001
 const app = express();
 
 app.post("/signup", (req, res) => {
+
+    const data = CreateUserSchema.safeParse(req.body)
+    if(!data.success) {
+        res.json({
+            message: "Invalid inputs"
+        })
+        return 
+    }
     //TODO: db call
     res.json({
         userId: "123"
@@ -14,6 +23,14 @@ app.post("/signup", (req, res) => {
 })
 
 app.post("/signin", (req, res) => {
+
+    const data = SigninSchema.safeParse(req.body)
+    if(!data.success) {
+        res.json({
+            message: "Invalid inputs"
+        })
+        return 
+    }
 
     const userId = 123;
     const token = jwt.sign({
@@ -27,6 +44,14 @@ app.post("/signin", (req, res) => {
 })
 
 app.post("/room", middleware, (req, res) => {
+
+    const data = CreateRoomSchema.safeParse(req.body)
+    if(!data.success) {
+        res.json({
+            message: "Invalid inputs"
+        })
+        return 
+    }
     //TODO: db call
     res.json({
         roomId: 123
